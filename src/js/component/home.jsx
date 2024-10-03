213cd4b82ae7a6a9025d3f90b789c17d1d0ea931
Joaquin Morales <Mintinvestments95@gmail.com>
2024-10-03 23:00:44 +0000

    Need to complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState ([]);
+	const [taskInput, setTaskInput] = useState ("");
+
+	const addTask = () => {
+		if (taskInput.trim () !== "") {
+			setTasks([...tasks, taskInput]);
+			setTaskInput("");
+		}
+	};
+
+	const handleKeyPress = (e) => {
+		if (e.key === "Enter") {
+			addTask();
+		}
+	};
+
+	const deleteTask = (index) => {
+		const updatedTasks = tasks.filter((_,i) => i !== index);
+		setTasks(updatedTasks);
+	};
+	
 	return (
 		<div className="text-center">
 			
